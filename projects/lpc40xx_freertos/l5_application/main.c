9acd4d0f9821ce9772177986a09a1f27e6595876
Jhnatek <42760223+Jhnatek@users.noreply.github.com>
2021-03-15 20:27:11 -0700

    unable to finish
    the end of part 2 had lots of bugs

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -1,19 +1,56 @@
-#include <stdio.h>
-
 #include "FreeRTOS.h"
 #include "task.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "board_io.h"
 #include "common_macros.h"
 #include "periodic_scheduler.h"
+#include "queue.h"
 #include "sj2_cli.h"
 
 #include "uart_lab.h"
+// python nxp-programmer/flash.py
 
 void uart_read_task(void *p) {
+  char *test;
+  while (1) {
+    if (uart_lab__get_char_from_queue(&test, portMAX_DELAY)) { // took out & from test
+      fprintf(stderr, "Test back: %c\n ", test);               // port 28
+      vTaskDelay(500);
+    } else {
+      fprintf(stderr, "error in read task\n");
+    }
+  }
+}
+
+void uart_write_task(void *p) {
+  // char test[5] = "test-"; // dont use double quotes!!!
+  char test = 't';
+  while (1) {
+    for (int i = 0; i < 5; i++) {
+      uart_lab__polled_put(UART_2, test); // port 29
+      printf("Test sent: %c\n ", test);
+    }
+    vTaskDelay(500);
+  }
+}
+
+void main(void) {
+  uart_lab__init(UART_2, 96, 38400);
+  uart__enable_receive_interrupt(UART_2);
+  xTaskCreate(uart_read_task, "test1", 2048 / sizeof(void *), NULL, PRIORITY_LOW, NULL);
+  xTaskCreate(uart_write_task, "test2", 2048 / sizeof(void *), NULL, PRIORITY_LOW, NULL);
+
+  vTaskStartScheduler();
+}
+
+#if (0)
+part 1 void uart_read_task(void *p) {
   while (1) {
     char *test;
-    uart_lab__polled_get(UART_3, &test); // port 01
+    uart_lab__polled_get(UART_2, &test); // port 28
     printf("Test back: %c\n ", test);
     vTaskDelay(500);
   }
@@ -21,8 +58,8 @@ void uart_read_task(void *p) {
 
 void uart_write_task(void *p) {
   while (1) {
-    char test = "t";
-    uart_lab__polled_put(UART_3, test); // port 00
+    char test = 't';                    // dont use double quotes!!!
+    uart_lab__polled_put(UART_2, test); // port 29
     printf("Test sent: %c\n ", test);
     vTaskDelay(500);
   }
@@ -31,9 +68,10 @@ void uart_write_task(void *p) {
 void main(void) {
   // TODO: Use uart_lab__init() function and initialize UART2 or UART3 (your choice)
   // TODO: Pin Configure IO pins to perform UART2/UART3 function
-  uart_lab__init(UART_3, 96, 38400);
+  uart_lab__init(UART_2, 96, 38400);
   xTaskCreate(uart_read_task, "test1", 2048 / sizeof(void *), NULL, PRIORITY_LOW, NULL);
   xTaskCreate(uart_write_task, "test2", 2048 / sizeof(void *), NULL, PRIORITY_LOW, NULL);
 
   vTaskStartScheduler();
 }
+#endif
--- projects/lpc40xx_freertos/l5_application/uart_lab.c
+++ projects/lpc40xx_freertos/l5_application/uart_lab.c
@@ -7,20 +7,27 @@
 #include "task.h"
 #include <stdio.h>
 
+static QueueHandle_t your_uart_rx_queue;
+
+// part 0
 static void uart_lab__baud_init(uart_number_e uart, uint32_t peripheral_clock, uint32_t baud_rate) {
   uint32_t temp_clock = (peripheral_clock * 1000 * 1000); // some reason it doesnt like temp_clock, but it works
   const uint16_t sixteen_bit = temp_clock / (16 * baud_rate);
   if (uart == UART_2) {
+    LPC_UART2->LCR |= (1 << 7);                 // dlab
     LPC_UART2->FDR = (0 << 0) | (1 << 4);       // divval|mulval
     LPC_UART2->DLL = (sixteen_bit & 0xFF);      // lower 8 bit values
     LPC_UART2->DLM = (sixteen_bit >> 8) & 0xFF; // upper 8 bit values
+    LPC_UART2->LCR &= ~(1 << 7);                // disable dlab (after this you dont use this, only thr and rbr)
 
   } else if (uart == UART_3) {
+    LPC_UART3->LCR |= (1 << 7);                 // dlab
     LPC_UART3->FDR = (0 << 0) | (1 << 4);       // divval|mulval
     LPC_UART3->DLL = (sixteen_bit & 0xFF);      // lower 8 bit values
     LPC_UART3->DLM = (sixteen_bit >> 8) & 0xFF; // upper 8 bit values
+    LPC_UART3->LCR &= ~(1 << 7);                // disable dlab (after this you dont use this, only thr and rbr)i
   } else {
-    fprintf(stderr, "Error: not UART 1 or 2 (baud funct)\n");
+    fprintf(stderr, "Error: not UART 2 or 3 (baud funct)\n");
   }
 }
 
@@ -36,7 +43,8 @@ static void uart_lab__iocon_init(uart_number_e uart) {
     LPC_IOCON->P0_1 &= ~(0b111); // clear
     LPC_IOCON->P0_0 |= (0b010);  // U3_TXD in
     LPC_IOCON->P0_1 |= (0b010);  // U3_RXD out
-    fprintf(stderr, "Error: not UART 1 or 2 iocon\n");
+  } else {
+    fprintf(stderr, "Error: not UART 2 or 3 iocon\n");
   }
 }
 
@@ -44,54 +52,122 @@ void uart_lab__init(uart_number_e uart, uint32_t peripheral_clock, uint32_t baud
 
   if (uart == UART_2) {
     LPC_SC->PCONP |= (1 << 24);   // uart 2 pconp (only real difference)
-    LPC_UART2->LCR |= (1 << 7);   // dlab
     LPC_UART2->LCR |= (0x3 << 0); // select length to be 8 bit using WLS
     uart_lab__iocon_init(uart);
     uart_lab__baud_init(uart, peripheral_clock, baud_rate);
 
-    LPC_UART2->LCR &= ~(1 << 7); // disable dlab (after this you dont use this, only thr and rbr)
-
   } else if (uart == UART_3) {
     LPC_SC->PCONP |= (1 << 25);   // uart 3 pconp (only real difference)
-    LPC_UART3->LCR |= (1 << 7);   // dlab
     LPC_UART3->LCR |= (0x3 << 0); // select length to be 8 bit using WLS
     uart_lab__iocon_init(uart);
     uart_lab__baud_init(uart, peripheral_clock, baud_rate);
-
-    LPC_UART3->LCR &= ~(1 << 7); // disable dlab (after this you dont use this, only thr and rbr)
   } else {
-    fprintf(stderr, "Error: not UART 1 or 2 \n");
+    fprintf(stderr, "Error: not UART 2 or 3 \n");
   }
 }
 
 bool uart_lab__polled_get(uart_number_e uart, char *input_byte) {
   if (uart == UART_2) {
-    bool checkempty = (LPC_UART2->LSR & (1 << 5));
-    if (checkempty) {
-      *input_byte = LPC_UART2->RBR;
+    bool checkempty = (LPC_UART2->LSR & (1 << 0));
+    // if (checkempty) {
+    //   *input_byte = LPC_UART2->RBR;
+    // }
+    while (!checkempty) {
+      ;
     }
+    *input_byte = LPC_UART2->RBR;
   }
 
-  if (uart == UART_3) {
-    bool checkempty = (LPC_UART3->LSR & (1 << 5));
-    if (checkempty) {
-      *input_byte = LPC_UART3->RBR;
+  else if (uart == UART_3) {
+    bool checkempty = (LPC_UART3->LSR & (1 << 0));
+    // if (checkempty) {
+    //   *input_byte = LPC_UART3->RBR;
+    // }
+    while (!checkempty) {
+      ;
     }
+    *input_byte = LPC_UART3->RBR;
+  } else {
+    fprintf(stderr, "Error: not UART 2 or 3 polled get\n");
   }
 }
 
 bool uart_lab__polled_put(uart_number_e uart, char output_byte) {
   if (uart == UART_2) {
     bool checkempty = (LPC_UART2->LSR & (1 << 5));
-    if (checkempty) {
-      LPC_UART2->THR = output_byte;
+    // if (checkempty) {
+    //   LPC_UART2->THR = output_byte;
+    // }
+    while (!checkempty) {
+      ;
     }
+    LPC_UART2->THR = output_byte;
   }
 
-  if (uart == UART_3) {
+  else if (uart == UART_3) {
     bool checkempty = (LPC_UART3->LSR & (1 << 5));
-    if (checkempty) {
-      LPC_UART3->THR = output_byte;
+    // if (checkempty) {
+    //   LPC_UART3->THR = output_byte;
+    // }
+    while (!checkempty) {
+      ;
+    }
+    LPC_UART3->THR = output_byte;
+  } else {
+    fprintf(stderr, "Error: not UART 2 or 3 polled put\n");
+  }
+}
+
+// lab part 2
+static void uart3_receive_interrupt(void) {
+  bool checkIIR = (LPC_UART3->IIR & (0x2 << 0));
+  bool check_LSR = (LPC_UART3->LSR & (0x1 << 0));
+  if (checkIIR) {
+    if (check_LSR) {
+      const char byte = LPC_UART3->RBR;
+      xQueueSendFromISR(your_uart_rx_queue, &byte, NULL);
     }
   }
+}
+
+static void uart2_receive_interrupt(void) {
+  fprintf(stderr, "uart2 interrupt received \n");
+  bool checkIIR = (!(LPC_UART2->IIR & (0x0 << 0)));
+  bool check_LSR = (LPC_UART2->LSR & (0x1 << 0));
+  if (checkIIR) {
+    fprintf(stderr, "uart2 interrupt received: checkIRR passed \n");
+    if (LPC_UART2->LSR & (0x1 << 0)) {
+      fprintf(stderr, "uart2 interrupt received: check_lsr passed \n");
+      const char byte = LPC_UART2->RBR;
+      xQueueSendFromISR(your_uart_rx_queue, &byte, NULL);
+    }
+  }
+}
+
+void uart__enable_receive_interrupt(uart_number_e uart_number) {
+  if (uart_number == UART_2) {
+    NVIC_EnableIRQ(UART2_IRQn);
+    lpc_peripheral__enable_interrupt(LPC_PERIPHERAL__UART2, uart2_receive_interrupt, NULL);
+    // LPC_UART2->IER |= (0b11);
+    LPC_UART2->IER |= (1 << 0); // RBR interrupt enable
+    LPC_UART2->IER |= (1 << 1); // THR interrupt enable
+  }
+
+  else if (uart_number == UART_3) {
+    NVIC_EnableIRQ(UART3_IRQn);
+    lpc_peripheral__enable_interrupt(LPC_PERIPHERAL__UART3, uart3_receive_interrupt, NULL);
+    LPC_UART2->IER |= (0b11);
+    // LPC_UART3->IER |= (1 << 0); // RBR interrupt enable
+    // LPC_UART3->IER |= (1 << 1); // THR interrupt enable
+  } else {
+    fprintf(stderr, "Error: not UART 2 or 3 receive interrupt\n");
+  }
+  // TODO: Create your RX queue
+  your_uart_rx_queue = xQueueCreate(5, sizeof(char));
+}
+
+// Public function to get a char from the queue (this function should work without modification)
+// TODO: Declare this at the header file
+bool uart_lab__get_char_from_queue(char *input_byte, uint32_t timeout) {
+  return xQueueReceive(your_uart_rx_queue, input_byte, timeout);
 }
